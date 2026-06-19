aaa9efdeda8c96d4e23212ac7ce97ac187c74b37
Student Apnacollege <amogh.joharisada@gmail.com>
2026-06-19 19:09:47 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,42 @@ import java.io.*;
 
 public class Main {
 
+    private static List<String> parseCommand(String input) {
+
+        List<String> tokens = new ArrayList<>();
+
+        StringBuilder current = new StringBuilder();
+
+        boolean inSingleQuote = false;
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char ch = input.charAt(i);
+
+            if (ch == '\'') {
+                inSingleQuote = !inSingleQuote;
+                continue;
+            }
+
+            if (Character.isWhitespace(ch) && !inSingleQuote) {
+
+                if (current.length() > 0) {
+                    tokens.add(current.toString());
+                    current.setLength(0);
+                }
+
+            } else {
+                current.append(ch);
+            }
+        }
+
+        if (current.length() > 0) {
+            tokens.add(current.toString());
+        }
+
+        return tokens;
+    }
+
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
@@ -24,35 +60,48 @@ public class Main {
                 System.out.println(currentDirectory.getAbsolutePath());
             }
 
-          else if(command.startsWith("cd ")) {
-            
+            else if (command.startsWith("cd ")) {
 
-    String path = command.substring(3);
-    if(path.equals("~")) {
-    path = System.getenv("HOME");
-}
+                String path = command.substring(3);
 
-    File target;
+                if (path.equals("~")) {
+                    path = System.getenv("HOME");
+                }
 
-    if(path.startsWith("/")) {
-        target = new File(path);
-    } else {
-        target = new File(currentDirectory, path);
-    }
+                File target;
 
-    target = target.getCanonicalFile();
+                if (path.startsWith("/")) {
+                    target = new File(path);
+                } else {
+                    target = new File(currentDirectory, path);
+                }
 
-    if(target.exists() && target.isDirectory()) {
-        currentDirectory = target;
-    } else {
-        System.out.println(
-            "cd: " + path + ": No such file or directory"
-        );
-    }
-}
+                target = target.getCanonicalFile();
 
-            else if (command.startsWith("echo ")) {
-                System.out.println(command.substring(5));
+                if (target.exists() && target.isDirectory()) {
+                    currentDirectory = target;
+                } else {
+                    System.out.println(
+                            "cd: " + path + ": No such file or directory");
+                }
+            }
+
+            else if (command.startsWith("echo")) {
+
+                List<String> tokens = parseCommand(command);
+
+                StringBuilder output = new StringBuilder();
+
+                for (int i = 1; i < tokens.size(); i++) {
+
+                    if (i > 1) {
+                        output.append(" ");
+                    }
+
+                    output.append(tokens.get(i));
+                }
+
+                System.out.println(output);
             }
 
             else if (command.startsWith("type ")) {
@@ -99,9 +148,13 @@ public class Main {
 
             else {
 
-                String[] parts = command.split("\\s+");
+                List<String> parts = parseCommand(command);
 
-                String cmd = parts[0];
+                if (parts.isEmpty()) {
+                    continue;
+                }
+
+                String cmd = parts.get(0);
 
                 String path = System.getenv("PATH");
                 String[] dirs = path.split(":");
@@ -127,9 +180,7 @@ public class Main {
                 List<String> commandList =
                         new ArrayList<>();
 
-                for (String part : parts) {
-                    commandList.add(part);
-                }
+                commandList.addAll(parts);
 
                 ProcessBuilder pb =
                         new ProcessBuilder(commandList);
