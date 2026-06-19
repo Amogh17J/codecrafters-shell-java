990a94ece3a5806b972d8c19b41cf38d109c5c14
Student Apnacollege <amogh.joharisada@gmail.com>
2026-06-19 18:16:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -64,8 +68,65 @@ public class Main {
 
             // Invalid command
             else {
-                System.out.println(command + ": command not found");
+
+    String[] parts = command.split("\\s+");
+    String cmd = parts[0];
+
+    String path = System.getenv("PATH");
+    String[] directories = path.split(":");
+
+    boolean found = false;
+
+    for (String dir : directories) {
+
+        File file = new File(dir, cmd);
+
+        if (file.exists() && file.canExecute()) {
+
+            found = true;
+
+            try {
+
+                java.util.List<String> commandList =
+                        new java.util.ArrayList<>();
+
+                commandList.add(file.getAbsolutePath());
+
+                for (int i = 1; i < parts.length; i++) {
+                    commandList.add(parts[i]);
+                }
+
+                ProcessBuilder pb =
+                        new ProcessBuilder(commandList);
+
+                Process process = pb.start();
+
+                java.io.BufferedReader reader =
+                        new java.io.BufferedReader(
+                                new java.io.InputStreamReader(
+                                        process.getInputStream()));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                process.waitFor();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+
+            break;
+        }
+    }
+
+    if (!found) {
+        System.out.println(command + ": command not found");
+    }
+}
+
         }
     }
 }
