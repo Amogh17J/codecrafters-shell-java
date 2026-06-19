d8163854bd37d27f167ce674a51460a3576bb9d0
Student Apnacollege <amogh.joharisada@gmail.com>
2026-06-19 18:11:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,68 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+
             System.out.print("$ ");
 
             String command = sc.nextLine();
 
-            if (command.startsWith("exit")) {
+            // exit builtin
+            if (command.equals("exit 0") || command.equals("exit")) {
                 break;
             }
 
+            // echo builtin
             else if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            } 
+            }
+
+            // type builtin
             else if (command.startsWith("type ")) {
+
                 String cmd = command.substring(5);
 
-                if(cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")){
+                // Check builtins first
+                if (cmd.equals("echo")
+                        || cmd.equals("exit")
+                        || cmd.equals("type")) {
+
                     System.out.println(cmd + " is a shell builtin");
                 }
-                else{
-                    System.out.println(cmd + " not found");
+
+                else {
+
+                    String path = System.getenv("PATH");
+                    String[] directories = path.split(":");
+
+                    boolean found = false;
+
+                    for (String dir : directories) {
+
+                        File file = new File(dir, cmd);
+
+                        if (file.exists() && file.canExecute()) {
+
+                            System.out.println(
+                                    cmd + " is " + file.getAbsolutePath());
+
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(cmd + ": not found");
+                    }
                 }
             }
+
+            // Invalid command
             else {
                 System.out.println(command + ": command not found");
             }
